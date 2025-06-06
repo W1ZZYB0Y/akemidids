bd228bb6289d3fd87352b4e92277e965e7fe6a1a
W1ZZYB0Y <udomwisdom466@gmail.com>
2025-06-06 11:26:03 -0700

    Update FriendsPage.js

--- src/pages/FriendsPage.js
+++ src/pages/FriendsPage.js
@@ -35,4 +35,41 @@ function FriendsPage() {
   );
 }
 
+export default FriendsPage;import React, { useEffect, useState } from 'react';
+import BottomNav from '../components/BottomNav';
+import './FriendsPage.css';
+
+function FriendsPage() {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const tgUser = window.Telegram?.WebApp?.initDataUnsafe?.user;
+    if (tgUser) {
+      setUsername(tgUser.username || tgUser.id);
+    }
+  }, []);
+
+  const referralLink = https://akemidids.vercel.app/?ref=${username};
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(referralLink);
+    alert('Referral link copied!');
+  };
+
+  return (
+    <div className="friends-page">
+      <h2 className="friends-title">Invite Your Friends</h2>
+      <p className="friends-text">
+        Share your referral link below and earn rewards when your friends join and start clicking!
+      </p>
+      <div className="referral-box">
+        <input type="text" value={referralLink} readOnly className="code" />
+        <button onClick={copyToClipboard} className="copy-button">Copy</button>
+      </div>
+
+      <BottomNav />
+    </div>
+  );
+}
+
 export default FriendsPage;
