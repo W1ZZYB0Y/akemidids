73504075a600da5862d5ab6c6848770f29064ae3
W1ZZYB0Y <udomwisdom466@gmail.com>
2025-06-06 09:58:14 -0700

    Update FriendsPage.js

--- src/pages/FriendsPage.js
+++ src/pages/FriendsPage.js
@@ -1,23 +1,23 @@
-// src/pages/FriendsPage.js
 import React, { useEffect, useState } from 'react';
 import BottomNav from '../components/BottomNav';
 import './FriendsPage.css';
 
 function FriendsPage() {
-  const [referralLink, setReferralLink] = useState('');
+  const [username, setUsername] = useState(null);
 
   useEffect(() => {
-    const user = window?.Telegram?.WebApp?.initDataUnsafe?.user;
-
-    if (user?.username) {
-      setReferralLink(`https://akemidids.vercel.app/?ref=${user.username}`);
-    } else {
-      setReferralLink('Telegram user not found');
+    const tgUser = window.Telegram?.WebApp?.initDataUnsafe?.user;
+    if (tgUser) {
+      setUsername(tgUser.username || tgUser.id);
     }
   }, []);
 
+  const referralLink = username
+    ? https://akemidids.vercel.app/?ref=${username}
+    : 'Telegram user not found';
+
   const copyToClipboard = () => {
-    if (referralLink && referralLink !== 'Telegram user not found') {
+    if (referralLink !== 'Telegram user not found') {
       navigator.clipboard.writeText(referralLink);
       alert('Referral link copied!');
     }
@@ -30,8 +30,8 @@ function FriendsPage() {
         Share your referral link below and earn rewards when your friends join and start clicking!
       </p>
       <div className="referral-box">
-        <input type="text" value={referralLink} readOnly className='code' />
-        <button onClick={copyToClipboard} className='copy-button'>Copy</button>
+        <input type="text" value={referralLink} readOnly className="code" />
+        <button onClick={copyToClipboard} className="copy-button">Copy</button>
       </div>
 
       <BottomNav />
