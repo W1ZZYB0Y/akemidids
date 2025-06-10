b5f27eac043d9f434e61572677a828f78d623fcb
W1ZZYB0Y <udomwisdom466@gmail.com>
2025-06-10 15:49:39 -0700

    Update userApi.js

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -13,13 +13,13 @@ export const completeTask = async (data) => {
   }
 };
 
-
-
+// Get user profile
 export const getUserProfile = async (telegramId) => {
   const res = await axios.get(`${BASE_URL}/profile/${telegramId}`);
   return res.data;
 };
 
+// Update Telegram username
 export const updateUsername = async (telegramId, username) => {
   const res = await axios.post(`${BASE_URL}/update-username`, { telegramId, username });
   return res.data;
